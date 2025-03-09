86ca522b9e4274e933aaa10c3e9b012e63b60162
200275zhx <200275zhx@gmail.com>
2025-03-09 18:52:59 -0400

    add some info in bio page, need more

--- src/app/[locale]/(content)/biography/page.tsx
+++ src/app/[locale]/(content)/biography/page.tsx
@@ -24,7 +24,7 @@ export default async function BiographyPage() {
         {/* selfie image */}
         <div className="relative">
           <Image
-            src="/assets/biopage/zhangzikangbio.webp"
+            src="/assets/biopage/zhangzikang-bio.webp"
             alt={t('name')}
             fill
             className="object-contain"
@@ -37,32 +37,57 @@ export default async function BiographyPage() {
 
         {/* education */}
         <div className='pt-4 text-2xl my-12'>{t('education-title')}</div>
-        {t.rich('education-content.[Year]', { div: (chunks) => <div>{chunks}</div> })}
+        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_10fr]'>
+          <div>1985-1989</div>
+          {t('education-content.1985-1989')}
+          <div>1990-1996</div>
+          {t('education-content.1990-1996')}
+          <div>2011-2014</div>
+          {t('education-content.2011-2014')}
+          
+        </div>
 
         {/* career */}
         <div className='pt-4 text-2xl my-12'>{t('career-title')}</div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_12fr] gap-4'>
-          <div>[年份]</div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_10fr]'>
+          <div>2024</div>
+          {t('career-content.2024')}
+          <div>2017-2024</div>
+          {t('career-content.2017-2024')}
+          <div>2014-2017</div>
+          {t('career-content.2014-2017')}
+          <div>2009-2014</div>
+          {t('career-content.2009-2014')}
+          <div>2009-2009</div>
+          {t('career-content.2009-2009')}
+          <div>2006-2009</div>
+          {t.rich('career-content.2006-2009', { div: (chunks) => <div>{chunks}</div> })}
+          <div>2004-2011</div>
+          {t('career-content.2004-2011')}
+          <div>1999-2006</div>
+          {t('career-content.1999-2006')}
+          <div>1992-1999</div>
+          {t.rich('career-content.1992-1999', { div: (chunks) => <div>{chunks}</div> })}
+          <div>1989-1992</div>
+          {t('career-content.1989-1992')}
+          <br/><br/> 
+          <div>[年份缺失]</div>
           <div>
             <div>{t.rich('career-content.[Year1]', { div: (chunks) => <div>{chunks}</div> })}</div>
             <div>{t.rich('career-content.[Year2]', { div: (chunks) => <div>{chunks}</div> })}</div>
             <div>{t.rich('career-content.[Year3]', { div: (chunks) => <div>{chunks}</div> })}</div>
           </div>
-          <div>[年份]</div>
+          <div>[年份缺失]</div>
           <div>
-            <div>{t.rich('career-content.[Year4]', { div: (chunks) => <div>{chunks}</div> })}</div>
             <div>{t.rich('career-content.[Year5]', { div: (chunks) => <div>{chunks}</div> })}</div>
             <div>{t.rich('career-content.[Year6]', { div: (chunks) => <div>{chunks}</div> })}</div>
-            <div>{t.rich('career-content.[Year7]', { div: (chunks) => <div>{chunks}</div> })}</div>
             <div>{t.rich('career-content.[Year8]', { div: (chunks) => <div>{chunks}</div> })}</div>
-            <div>{t.rich('career-content.[Year9]', { div: (chunks) => <div>{chunks}</div> })}</div>
-            <div>{t.rich('career-content.[Year0]', { div: (chunks) => <div>{chunks}</div> })}</div>
           </div>
         </div>
 
         {/* solo exhibitions */}
         <div className='pt-4 text-2xl my-12'>{t('solo-exhibitions-title')}</div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_12fr] gap-4'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_10fr]'>
           <div>2024</div>
           <div>{t.rich('solo-exhibitions-content.2024', { div: (chunks) => <div>{chunks}</div> })}</div>
           <div>2022</div>
@@ -71,7 +96,7 @@ export default async function BiographyPage() {
 
         {/* group exhibitions */}
         <div className='pt-4 text-2xl my-12'>{t('group-exhibitions-title')}</div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_12fr] gap-4'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_10fr]'>
           <div>2025</div>
           <div>{t.rich('group-exhibitions-content.2025', { div: (chunks) => <div>{chunks}</div> })}</div>
           <div>2024</div>
@@ -80,6 +105,8 @@ export default async function BiographyPage() {
           <div>{t.rich('group-exhibitions-content.2023', { div: (chunks) => <div>{chunks}</div> })}</div>
           <div>2022</div>
           <div>{t.rich('group-exhibitions-content.2022', { div: (chunks) => <div>{chunks}</div> })}</div>
+          <div>2021</div>
+          <div>{t.rich('group-exhibitions-content.2021', { div: (chunks) => <div>{chunks}</div> })}</div>
           <div>2020</div>
           <div>{t.rich('group-exhibitions-content.2020', { div: (chunks) => <div>{chunks}</div> })}</div>
           <div>2017</div>
@@ -88,18 +115,18 @@ export default async function BiographyPage() {
 
         {/* public collections */}
         <div className='pt-4 text-2xl my-12'>{t('public-collections-title')}</div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_12fr] gap-4'>
-          <div>[年份]</div>
-          <div>{t.rich('public-collections-content.[Year1]', { div: (chunks) => <div>{chunks}</div> })}</div>
-          <div>[年份]</div>
-          <div>{t.rich('public-collections-content.[Year2]', { div: (chunks) => <div>{chunks}</div> })}</div>
-          <div>[年份]</div>
-          <div>{t.rich('public-collections-content.[Year3]', { div: (chunks) => <div>{chunks}</div> })}</div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_10fr]'>
+          <div>2023</div>
+          <div>{t.rich('public-collections-content.2023', { div: (chunks) => <div>{chunks}</div> })}</div>
+          <div>2018</div>
+          <div>{t.rich('public-collections-content.2018', { div: (chunks) => <div>{chunks}</div> })}</div>
+          <div>2016</div>
+          <div>{t.rich('public-collections-content.2016', { div: (chunks) => <div>{chunks}</div> })}</div>
         </div>
 
         {/* curations */}
         <div className='pt-4 text-2xl my-12'>{t('curations-title')}</div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_12fr] gap-4'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_10fr]'>
           <div>2024</div>
           <div>{t.rich('curations-content.2024', { div: (chunks) => <div>{chunks}</div> })}</div>
           <div>2023</div>
@@ -113,13 +140,21 @@ export default async function BiographyPage() {
           <div>2019</div>
           <div>{t.rich('curations-content.2019', { div: (chunks) => <div>{chunks}</div> })}</div>
           <div>2018</div>
-          <div>{t.rich('curations-content.2018', { div: (chunks) => <div>{chunks}</div> })}</div>
+          <div>{t.rich('curations-content.2018', { div: (chunks) => <div>{chunks}</div> })}</div> 
+          <div>2016</div>
+          <div>{t.rich('curations-content.2016', { div: (chunks) => <div>{chunks}</div> })}</div>
+          <div>2015</div>
+          <div>{t.rich('curations-content.2015', { div: (chunks) => <div>{chunks}</div> })}</div>
+          <div>2012</div>
+          <div>{t.rich('curations-content.2012', { div: (chunks) => <div>{chunks}</div> })}</div>
+          <div>2011</div>
+          <div>{t.rich('curations-content.2011', { div: (chunks) => <div>{chunks}</div> })}</div>
         </div>
 
         {/* awards */}
         <div className='pt-4 text-2xl my-12'>{t('awards-title')}</div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_12fr] gap-4'>
-          <div>[年份]</div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-[1fr_10fr]'>
+          <div>[年份缺失]</div>
           <div>{t.rich('awards-content.[Year]', { div: (chunks) => <div>{chunks}</div> })}</div>
         </div>
 
